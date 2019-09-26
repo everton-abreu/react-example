0aa56fd2d644580e680275f8fbaf86d8a8280cff
Everton Junior de Abreu <everton@jrabreu.com>
2019-09-26 14:04:18 -0300

    add eslint to format code

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,9 @@
 import React from 'react';
 
-const App = () => {
-
-	return (
-		<>
-			<h1>Ola Mundo</h1>
-		</>
-	);
-}
+const App = () => (
+  <>
+    <h1>Ola Mundo</h1>
+  </>
+);
 
 export default App;
