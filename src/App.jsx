d2e91f101ae55b7e24e02173dba5dbbbf8f145eb
Everton Junior de Abreu <everton@jrabreu.com>
2019-10-13 01:35:58 -0300

    Exemplo de home com full size

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,14 @@ import PrivatedRoute from './utils/privatedRoute';
 import { HelloWorld } from './components';
 
 import Home from './pages/home';
+import HomeWithContent from './pages/home2';
 
 export default function App() {
   return (
     <BrowserRouter>
       <Switch>
         <Route exact path="/" component={Home} />
+        <Route exact path="/home2" component={HomeWithContent} />
         <PrivatedRoute path="/**" component={HelloWorld} redirectTo="/" />
       </Switch>
     </BrowserRouter>
